cf8d0bee0af9d59771be62fa0e77a5f3acffcedb
Ludhol <ludvig.holmer@gmail.com>
2021-02-01 18:22:19 +0100

    try catch update

--- dist/App.js
+++ dist/App.js
@@ -9,6 +9,7 @@ function App({}) {
   const FileData = (props) => {
     const files2 = props.files;
     const [data, setData] = useState(null);
+    const [countErrors, setErrors] = useState([]);
     const onFileUpload = () => {
       const formData = new FormData();
       if (files2) {
@@ -16,7 +17,7 @@ function App({}) {
           const file = files2.item(index);
           if (file) {
             formData.append("myFile", file, file.name);
-            (async () => setData(await boilBlood(file, [])))();
+            (async () => setData(await boilBlood(file, [], countErrors, setErrors)))();
           }
           ;
         }
@@ -27,30 +28,41 @@ function App({}) {
     const ZippedData = () => {
       if (data) {
         const meta_data = haruspicy(data);
-        const delimiter = "|";
-        const headers = Object.keys(meta_data[0]).join(delimiter);
-        const body = meta_data.map((j) => Object.values(j).join(delimiter)).join("\n");
-        const str = headers + "\n" + body;
-        const bytes = new TextEncoder().encode(str);
-        const blob = new Blob([bytes], {
-          type: "application/json;charset=utf-8"
-        });
-        const objectUrl = URL.createObjectURL(blob);
-        const date = new Date();
-        const date_output = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
-        return /* @__PURE__ */ React.createElement("div", {
-          className: "App"
-        }, /* @__PURE__ */ React.createElement("h2", null, "Unzipped Details:"), /* @__PURE__ */ React.createElement("p", null, " Number of xlsx files found: ", meta_data.length, " "), /* @__PURE__ */ React.createElement("a", {
-          href: objectUrl,
-          target: "_blank",
-          rel: "noopener noreferrer",
-          download: `xlsx_meta_${date_output}.csv`
-        }, /* @__PURE__ */ React.createElement("button", null, "Download data")), /* @__PURE__ */ React.createElement(DataTable, {
-          data: meta_data
-        }));
-      } else {
-        return null;
+        console.log("data exists", meta_data, data);
+        if (meta_data[0]) {
+          console.log("meta_data[0] exists");
+          const delimiter = "|";
+          const headers = Object.keys(meta_data[0]).join(delimiter);
+          const body = meta_data.map((j) => Object.values(j).join(delimiter)).join("\n");
+          const str = headers + "\n" + body;
+          const bytes = new TextEncoder().encode(str);
+          const blob = new Blob([bytes], {
+            type: "application/json;charset=utf-8"
+          });
+          const objectUrl = URL.createObjectURL(blob);
+          const date = new Date();
+          const date_output = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+          return /* @__PURE__ */ React.createElement("div", {
+            className: "App"
+          }, /* @__PURE__ */ React.createElement("h2", null, "Unzipped Details:"), /* @__PURE__ */ React.createElement("p", null, "Number of xlsx files found: ", meta_data.length), /* @__PURE__ */ React.createElement("p", null, "Number of faulty zip files: ", countErrors.length), () => {
+            if (countErrors.length > 0) {
+              return countErrors.forEach((regNumb) => {
+                () => {
+                  return /* @__PURE__ */ React.createElement("p", null, "Uploaded faulty file: ", regNumb);
+                };
+              });
+            }
+          }, /* @__PURE__ */ React.createElement("a", {
+            href: objectUrl,
+            target: "_blank",
+            rel: "noopener noreferrer",
+            download: `xlsx_meta_${date_output}.csv`
+          }, /* @__PURE__ */ React.createElement("button", null, "Download data")), /* @__PURE__ */ React.createElement(DataTable, {
+            data: meta_data
+          }));
+        }
       }
+      return null;
     };
     if (files2) {
       return /* @__PURE__ */ React.createElement("div", {
--- dist/boilBlood.js
+++ dist/boilBlood.js
@@ -1,5 +1,5 @@
 import JSZip from "../web_modules/jszip.js";
-export default async function boilBlood(file, data) {
+export default async function boilBlood(file, data, countErrors, setErrors) {
   function renameFile(originalFile, newEnding, newType = originalFile.type) {
     const newName = originalFile.name.split(".")[0] + "." + newEnding;
     return new File([originalFile], newName, {
@@ -8,33 +8,40 @@ export default async function boilBlood(file, data) {
     });
   }
   if (file) {
-    const reg_numb = file.name.substring(0, 5);
-    if (file.name.split(".")[1] == "xlsx") {
+    const regNumb = file.name.substring(0, 5);
+    let fileEnding = file.name.split(".")[file.name.split(".").length - 1];
+    if (fileEnding == "xlsx") {
       file = renameFile(file, "zip");
+      fileEnding = "zip";
     }
-    if (file.name.split(".")[1] == "zip") {
+    if (fileEnding == "zip") {
       const new_zip = new JSZip();
-      const zip = await new_zip.loadAsync(file);
-      const zip_arr = [];
-      zip.forEach((relativePath, zipEntry) => {
-        zip_arr.push(zipEntry);
-      });
-      for (let index = 0; index < zip_arr.length; index++) {
-        const zipEntry = zip_arr[index];
-        const fileType = zipEntry.name.split(".")[1];
-        if (zipEntry.name == "docProps/core.xml") {
-          const core = await zipEntry.async("string");
-          data.push({
-            reg_numb,
-            core
-          });
-          return data;
-        } else if (fileType === "xlsx") {
-          const fileName = zipEntry.name.split("/")[zipEntry.name.split("/").length - 1];
-          const newBlob = await zipEntry.async("blob");
-          const newFile = new File([newBlob], fileName);
-          data = await boilBlood(newFile, data);
+      try {
+        const zip = await new_zip.loadAsync(file);
+        const zip_arr = [];
+        zip.forEach((_, zipEntry) => {
+          zip_arr.push(zipEntry);
+        });
+        for (let index = 0; index < zip_arr.length; index++) {
+          const zipEntry = zip_arr[index];
+          const fileType = zipEntry.name.split(".")[zipEntry.name.split(".").length - 1];
+          if (zipEntry.name == "docProps/core.xml") {
+            const core = await zipEntry.async("string");
+            data.push({
+              reg_numb: regNumb,
+              core
+            });
+            return data;
+          } else if (fileType === "xlsx") {
+            const fileName = zipEntry.name.split("/")[zipEntry.name.split("/").length - 1];
+            const newBlob = await zipEntry.async("blob");
+            const newFile = new File([newBlob], fileName);
+            data = await boilBlood(newFile, data, countErrors, setErrors);
+          }
         }
+      } catch (error) {
+        console.log(error);
+        setErrors(countErrors.push(regNumb));
       }
     }
   }
